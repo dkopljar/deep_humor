4661f809cf191ff21f5e13ba453dc7690df25bc2
Bartol Freškura <freskura.bartol@gmail.com>
2017-03-21 16:06:40 +0100

    Add custom download dir to download function

--- src/utils.py
+++ src/utils.py
@@ -29,7 +29,7 @@ def extract_zip(file, ext_dir):
         print("Could not delete file", file, e)
 
 
-def download_data():
+def download_data(download_dir="/tmp"):
     """
     Downloads datasets and resources required for the project
     :return:
@@ -37,14 +37,13 @@ def download_data():
     dir_creator([constants.DATA, constants.RSC])
 
     # glove
-    glove_name = os.path.join(constants.RSC, "glove.zip")
+    glove_name = os.path.join(download_dir, "glove.zip")
     if not os.path.exists(glove_name):
         print("Downloading Twitter Glove vector from", constants.GLOVE_TWITTER)
         print("This may take a while because the file size is 1.4GB")
         wget.download(constants.GLOVE_TWITTER, glove_name)
         print("Downloaded to", glove_name, "\n")
 
-    if os.path.emp
     extract_zip(glove_name, constants.RSC)
 
     # Dataset
@@ -52,7 +51,7 @@ def download_data():
     for link, file in zip([constants.TRAIN, constants.VALIDATION,
                            constants.VALIDATION_NO_LABELS],
                           ["train", "valid", "eval"]):
-        file = os.path.join(constants.DATA, file)
+        file = os.path.join(download_dir, file)
         if not os.path.exists(file):
             print("Downloading", link)
             wget.download(link, file)

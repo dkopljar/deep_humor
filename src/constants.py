167c02ae0a56e8f86f68af3c7da3bfec7d264547
Bartol Freškura <freskura.bartol@gmail.com>
2017-03-21 15:56:37 +0100

    Add function for downloading resources and dataset

--- src/constants.py
+++ src/constants.py
@@ -5,3 +5,9 @@ ROOT = os.path.dirname(__file__)
 RSC = os.path.join(ROOT, "resources")
 
 DATA = os.path.join(ROOT, "data")
+
+# DATA URLS
+GLOVE_TWITTER = "http://nlp.stanford.edu/data/glove.twitter.27B.zip"
+TRAIN = "http://alt.qcri.org/semeval2017/task6/data/uploads/train_data.zip"
+VALIDATION = "http://alt.qcri.org/semeval2017/task6/data/uploads/gold_data.zip"
+VALIDATION_NO_LABELS = "http://alt.qcri.org/semeval2017/task6/data/uploads/evaluation_dir.zip"
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import os
+import zipfile
 import constants
+import wget
 
 
 def dir_creator(dirs_list):
@@ -14,6 +16,45 @@ def dir_creator(dirs_list):
             print("Created directory", d)
 
 
-if __name__ == "__main__":
-    dir_list = [constants.DATA, constants.RSC]
-    dir_creator(dir_list)
+def extract_zip(file, ext_dir):
+    print("Extracting", file, "to", ext_dir)
+    with zipfile.ZipFile(file, "r") as zip_ref:
+        zip_ref.extractall(ext_dir)
+    print("Extraction finished!\n")
+
+    # Cleanup
+    try:
+        os.remove(file)
+    except Exception as e:
+        print("Could not delete file", file, e)
+
+
+def download_data():
+    """
+    Downloads datasets and resources required for the project
+    :return:
+    """
+    dir_creator([constants.DATA, constants.RSC])
+
+    # glove
+    glove_name = os.path.join(constants.RSC, "glove.zip")
+    if not os.path.exists(glove_name):
+        print("Downloading Twitter Glove vector from", constants.GLOVE_TWITTER)
+        print("This may take a while because the file size is 1.4GB")
+        wget.download(constants.GLOVE_TWITTER, glove_name)
+        print("Downloaded to", glove_name, "\n")
+
+    if os.path.emp
+    extract_zip(glove_name, constants.RSC)
+
+    # Dataset
+    print("Downloading dataset")
+    for link, file in zip([constants.TRAIN, constants.VALIDATION,
+                           constants.VALIDATION_NO_LABELS],
+                          ["train", "valid", "eval"]):
+        file = os.path.join(constants.DATA, file)
+        if not os.path.exists(file):
+            print("Downloading", link)
+            wget.download(link, file)
+
+        extract_zip(file, constants.DATA)

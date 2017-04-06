e07532e019c0c0f0df3675914a4b0dc53dbbe5ad
Damir <kopljar.damir@gmail.com>
2017-04-06 18:20:14 +0200

    Adding parser

--- src/dataset_parser.py
+++ src/dataset_parser.py
@@ -1,45 +1,98 @@
 import os
 import re
+import numpy as np
+import pickle
+import pdb
 
-TRAIN_PATH = "/Users/filipgulan/apt_project/dataset/train_data"
+data_path = "/Users/Dkopljar/Desktop/Faks/Diplomski/APT/apt_project/dataset/train_data"
+glove_file = "/Users/Dkopljar/Desktop/Faks/Diplomski/APT/apt_project/src/resources/glove/glove.twitter.27B.100d.txt"
 
-def read_file_by_line_and_tokenize(file_path):
-    tweets = [line.rstrip('\n') for line in open(file_path, 'r', encoding="utf8")]
-    tokenized = [re.split('\t', tweet) for tweet in tweets]
-    return tokenized
- 
-def filterText(text):  # remove unnecesary data from tweet, such as extra hashtags, links
-    tweets = []
-    for tweet in text:
+
+def filterText(tweets):  # remove unnecesary data from tweet, such as extra hashtags, links
+    result = []
+    for tweet in tweets:
         filtered = []
         for token in tweet:
             if(not(token.startswith('#') or token.startswith('@') or token.startswith('.@') or token.startswith('http'))):
                 filtered.append(token)
         while filtered.__contains__(''):
             filtered.remove('')
-        tweets.append(filtered)
-    return tweets
+        result.append(filtered)
+    return result
+
+def read_file_by_line_and_tokenize(file_path):
+    tweets = [line.rstrip('\n') for line in open(file_path, 'r', encoding="utf8")]
+    tokenized = [re.split('\t', tweet) for tweet in tweets]
+    return tokenized
 
 def combinantorial(lst):
-    count = 0
     index = 1
     pairs = []
     for element1 in lst:
         for element2 in lst[index:]:
-            if element1[2] == element2[2]:
+            if element1[1] == element2[1]:
                 continue
-            if element1[2] < element2[2]:
-                pairs.append((element1, element2))
+            if element1[1] < element2[1]:
+                if np.random.random() > 0.5:
+                    concatMatrix = np.concatenate((element1[0],element2[0]),axis=1)
+                    pairs.append((concatMatrix,0))
+                else:
+                    concatMatrix = np.concatenate((element2[0],element1[0]),axis=1)
+                    pairs.append((concatMatrix,1))
             else:
-                pairs.append((element2, element1))
+                if np.random.random() > 0.5:
+                    concatMatrix = np.concatenate((element2[0],element1[0]),axis=1)
+                    pairs.append((concatMatrix,0))
+                else:
+                    concatMatrix = np.concatenate((element1[0],element2[0]),axis=1)
+                    pairs.append((concatMatrix,1))
         index += 1
     return pairs
 
-all_combs = []
-for filename in os.listdir(TRAIN_PATH):
-    if filename.endswith(".tsv"):
-        current_rows = read_file_by_line_and_tokenize(os.path.join(TRAIN_PATH, filename))
-        comb = combinantorial(current_rows)
+def parse_data(glove_file,data_path,pickleDir):
+    embed_dict={}
+
+    print("Loading glove fle")
+    with open(glove_file) as f:
+            for line in f:
+                split = line.split()
+                token = split[0]
+                vec = np.array([np.float(x) for x in split[1:]])
+                embed_dict[token] = vec
+
+
+    all_combs = []
+    numberOfRows = 25
+    rowSize = 100
+    topicsMatrix=[]
+
+    paddingRow = np.zeros(rowSize)
+    for i,filename in enumerate(os.listdir(data_path)):
+        print("Parsing file number:",i)
+
+        if filename.endswith(".tsv"):
+            tweetsMatrix = []
+            current_rows = read_file_by_line_and_tokenize(os.path.join(data_path, filename))
+            ranks = [word[2] for word in current_rows]
+            tokenized = [re.split('\s| ', word[1]) for word in current_rows]
+            tokenized = filterText(tokenized)
+            for k,tweet in enumerate(tokenized):
+                sentenceRow = np.zeros((rowSize,numberOfRows))
+                for j,token in enumerate(tweet[:numberOfRows]):
+                    if(token in embed_dict):
+
+                        sentenceRow[:,j]=embed_dict[token.lower()]
+                tweetsMatrix.append((sentenceRow, ranks[k]))
+            topicsMatrix.append(tweetsMatrix)
+
+
+    for topic in topicsMatrix:
+        comb = combinantorial(topic)
         all_combs.extend(comb)
 
-print(len(all_combs))
+    with open(pickleDir,"wb") as f:
+        pickle.dump(all_combs, f)
+    
+
+
+parse_data(glove_file, data_path)
